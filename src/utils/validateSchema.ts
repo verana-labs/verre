18fddd1f60cba8021315b37b2765b1a41d2efe26
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-04-02 23:50:38 -0500

    fix: add validateSchemaContent
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -1,12 +1,10 @@
-import Ajv from 'ajv/dist/2020'
+import Ajv, { JSONSchemaType } from 'ajv/dist/2020'
 import addFormats from 'ajv-formats'
 
-import { ECS } from '../types'
+import { ECS, TrustErrorCode } from '../types'
 
 import { essentialSchemas } from './data'
-
-const ajv = new Ajv({ strict: false })
-addFormats(ajv)
+import { TrustError } from './trustError'
 
 export const loadSchema = (schemaName: keyof typeof essentialSchemas) => {
   return essentialSchemas[schemaName]
@@ -20,6 +18,8 @@ const schemas = {
 }
 
 export const identifySchema = (vp: any): ECS | null => {
+  const ajv = new Ajv({ strict: false })
+  addFormats(ajv)
   for (const schemaName of Object.keys(schemas) as ECS[]) {
     const validate = ajv.compile(schemas[schemaName].properties.credentialSubject)
     if (validate(vp)) {
@@ -36,4 +36,16 @@ export const checkSchemaMatch = (vp: any): ECS | null => {
     }
   }
   return null
+}
+
+export function validateSchemaContent<T>(schema: JSONSchemaType<T>, data: T): boolean {
+  const ajv = new Ajv({ strict: false })
+  addFormats(ajv)
+  const validate = ajv.compile(schema)
+  if (!validate(data))
+    throw new TrustError(
+      TrustErrorCode.SCHEMA_MISMATCH,
+      `Credential does not conform to schema: ${JSON.stringify(validate.errors)}`,
+    )
+  return true
 }
