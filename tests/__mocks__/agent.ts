213ede05713d9d3bd431ac6e21e51fbad3acae51
Ariel Gentile <gentilester@gmail.com>
2025-12-09 12:31:08 -0300

    fix: dependency issues in tests
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- tests/__mocks__/agent.ts
+++ tests/__mocks__/agent.ts
@@ -1,13 +1,12 @@
-import {
-  AskarModule,
-  AskarModuleConfigStoreOptions,
-  AskarSqliteConfig,
-  AskarSqliteStorageConfig,
-} from '@credo-ts/askar'
+// FIXME: Imports order once https://github.com/openwallet-foundation/askar-wrapper-javascript/issues/77
+// is solved
+// eslint-disable-next-line prettier/prettier, import/order
+import { askar, KdfMethod } from '@openwallet-foundation/askar-nodejs'
+
+import { AskarModule, AskarModuleConfigStoreOptions } from '@credo-ts/askar'
 import { Agent, DidsModule, utils, WebDidResolver } from '@credo-ts/core'
 import { agentDependencies } from '@credo-ts/node'
 import { WebVhDidResolver } from '@credo-ts/webvh'
-import { askar, KdfMethod } from '@openwallet-foundation/askar-nodejs'
 
 export const setupAgent = async ({ name }: { name: string }) => {
   const agent = new Agent({
@@ -15,7 +14,7 @@ export const setupAgent = async ({ name }: { name: string }) => {
     modules: {
       askar: new AskarModule({
         askar,
-        store: getAskarStoreConfig(name, { inMemory: true }),
+        store: getAskarStoreConfig(name),
       }),
       dids: new DidsModule({
         resolvers: [new WebDidResolver(), new WebVhDidResolver()],
@@ -57,21 +56,10 @@ export const setupAgent = async ({ name }: { name: string }) => {
  * });
  * ```
  */
-export function getAskarStoreConfig(
-  name: string,
-  {
-    inMemory = true,
-    random = utils.uuid().slice(0, 4),
-    maxConnections,
-  }: { inMemory?: boolean; random?: string; maxConnections?: number } = {},
-): AskarModuleConfigStoreOptions {
+export function getAskarStoreConfig(name: string): AskarModuleConfigStoreOptions {
   return {
-    id: `Wallet: ${name} - ${random}`,
+    id: `Wallet: ${name} - ${utils.uuid().slice(0, 4)}`,
     key: 'DZ9hPqFWTPxemcGea72C1X1nusqk5wFNLq6QPjwXGqAa',
     keyDerivationMethod: KdfMethod.Raw,
-    database: {
-      type: 'sqlite',
-      config: { inMemory, maxConnections } as AskarSqliteConfig,
-    } as AskarSqliteStorageConfig,
   }
 }
